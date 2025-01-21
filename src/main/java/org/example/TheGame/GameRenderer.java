4bf5dedb2bc42332a72cbaad31ded557565c6fc8
Simon Thomsson <simtho0624@gmail.com>
2025-01-21 11:06:34 +0100

    Move logic Draw forms new class

---
+++ src/main/java/org/example/TheGame/GameRenderer.java
@@ -0,0 +1,34 @@
+package org.example.TheGame;
+
+import java.awt.*;
+
+public class GameRenderer {
+
+    public void drawMainRectangle(Graphics g, int x, int y, int width, int height) {
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, width, height);
+    }
+
+    public void drawFollowingRectangle(Graphics g, int x, int y, int width, int height) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, width, height);
+    }
+
+    public void drawCircle(Graphics g, int x, int y, int diameter) {
+        g.setColor(Color.GREEN);
+        g.fillOval(x, y, diameter, diameter);
+    }
+
+    public void drawWall(Graphics g, int x, int y, int width, int height) {
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, width, height);
+    }
+
+    public void drawText(Graphics g, int elapsedTime, double followSpeed, int score) {
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.drawString("Time: " + elapsedTime + "s", 10, 20);
+        g.drawString("Hunter speed: " + String.format("%.1f", followSpeed), 10, 40);
+        g.drawString("Score: " + score, 10, 60);
+    }
+}
